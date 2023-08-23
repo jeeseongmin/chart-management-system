0f8471d2be994e499c5794d93980513631fcd109
jeeseongmin <peration0422@exntu.com>
2023-08-23 17:18:25 +0900

    Feat[#5] : nodejs 연결

--- server/server.js
+++ server/server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 
-app.use(express.static(path.join(__dirname, '/front/build')));
+app.use(express.static(path.join(__dirname, '../front/build')));
 
 app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, '/front/build/index.html'))
+  res.sendFile(path.join(__dirname, '../front/build/index.html'))
 })
 
 app.listen(8080, function () {
