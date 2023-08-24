a158691a00d1b5d51511395b108274bc5aa5a083
jeeseongmin <peration0422@exntu.com>
2023-08-24 13:57:56 +0900

    Feat[#6] : mongodb 설치 및 연결

--- server/server.js
+++ server/server.js
@@ -1,13 +1,32 @@
+const cors = require("cors");
 const express = require("express");
 const path = require("path");
 const app = express();
+require('dotenv').config();
+const mongoose = require("mongoose");
+console.log(process.env.DB_PASSWORD)
 
-app.use(express.static(path.join(__dirname, '../front/build')));
+const uri = `mongodb+srv://admin:${process.env.DB_PASSWORD}@system.pw6r10d.mongodb.net/?retryWrites=true&w=majority`
+mongoose.connect(uri)
+const db = mongoose.connection;
 
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, '../front/build/index.html'))
+db.on('error', function () {
+  console.log("Connection Failed!");
 })
+
+db.once("open", function () {
+  console.log("Connected!")
+})
+
+app.use(express.json());
+app.use(cors());
 
 app.listen(8080, function () {
   console.log('listening on 8080');
+})
+
+app.use(express.static(path.join(__dirname, '../front/build')));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(__dirname, '../front/build/index.html'))
 })
