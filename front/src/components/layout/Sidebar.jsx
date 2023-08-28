7ff32e6261fec73344eef3090767f58959a514ed
jeeseongmin <peration0422@exntu.com>
2023-08-28 11:16:37 +0900

    Feat[#7] : 사이드 바 컨텐츠 개발

--- front/src/components/layout/Sidebar.jsx
+++ front/src/components/layout/Sidebar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { IoMenu } from "react-icons/io5";
 import { useDispatch, useSelector } from "react-redux";
 import { toggleSidebar } from "../../features/setting/settingSlice";
+import { BsFillPersonFill } from "react-icons/bs";
 
 const Sidebar = () => {
   const dispatch = useDispatch();
@@ -16,15 +17,29 @@ const Sidebar = () => {
         <div><b><span style={{color: "white"}}>CHART</span> <span style={{color: "#a3a3a3"}}>SYSTEM</span></b></div>
       </div>
       <div id={"content"}>
-        <div>
-          <div>
-            <div className={"font-gray"}>
-              Chart
-            </div>
-            <div>하하</div>
+        {/* 기본 스페이스 */}
+        <div id={"workspace"}>
+          <div id={"spaceTitle"}>
+            WORKSPACE
           </div>
-        
+          <div id={"space"}>
+            <BsFillPersonFill size={16} style={{marginTop: "2px"}}/>
+            <span>Polaris Workspace</span>
+          </div>
         </div>
+        
+        {/* 즐겨찾기 스페이스 */}
+        <div id={"workspace"}>
+          <div id={"spaceSmallTitle"}>
+            즐겨찾기
+          </div>
+          <div id={"space"}>
+            <BsFillPersonFill size={16} style={{marginTop: "2px"}}/>
+            <span>교육용</span>
+          </div>
+        </div>
+        <button id={"spaceListButton"}>워크스페이스 목록</button>
+        
         <div id={""}></div>
       </div>
     </div>
