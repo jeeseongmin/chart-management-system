21b5d8b7ed42987556fd6a16e8c7cbbd7cb07561
jeeseongmin <peration0422@exntu.com>
2023-08-29 09:54:04 +0900

    Feat[#9] : 워크스페이스 헤더 개발

--- front/src/components/layout/ContentsLayout.jsx
+++ front/src/components/layout/ContentsLayout.jsx
@@ -10,6 +10,14 @@ const ContentLayout = ({children}) => {
   return (
     <div id={"contentLayout"}>
       <Sidebar/>
+      <div id={"workspaceHeader"}>
+        <div style={{display: "flex", alignItems: "center", gap: "16px", border: "1px solid red"}}>
+          <h1 style={{margin: 0, padding: 0}}>My WorkSpace</h1>
+          <div style={{color: "white", backgroundColor: "#35393f", fontWeight: "lighter", padding: "4px 12px"}}>소유자
+          </div>
+        </div>
+        <div></div>
+      </div>
       <Contents/>
     </div>
   );
