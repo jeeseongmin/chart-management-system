95e056a75486b6f5b371b86d5e470eb0dc5c18b2
jeeseongmin <peration0422@exntu.com>
2023-09-03 23:07:57 +0900

    Feat[#11] : Schema 구성

--- server/models/Chart.js
+++ server/models/Chart.js
@@ -1,12 +1,13 @@
 // 데이터베이스 스키마 저장
 const mongoose = require('mongoose');
 
-const WorkspaceSchema = new mongoose.Schema({
+const ChartSchema = new mongoose.Schema({
   id: Number,
   name: String,
   description: String,
+  config: Object, // 추후 인터페이스 생성
   createdAt: {type: Date, default: Date.now},
   updatedAt: {type: Date, default: Date.now},
-})
+});
 
-module.exports = mongoose.model('Workspace', WorkspaceSchema)
+module.exports = mongoose.model('Chart', ChartSchema)
--- server/models/User.js
+++ server/models/User.js
@@ -1,12 +1,13 @@
 // 데이터베이스 스키마 저장
 const mongoose = require('mongoose');
 
-const WorkspaceSchema = new mongoose.Schema({
+const UserSchema = new mongoose.Schema({
   id: Number,
   name: String,
-  description: String,
+  email: String,
+  password: String,
   createdAt: {type: Date, default: Date.now},
   updatedAt: {type: Date, default: Date.now},
 })
 
-module.exports = mongoose.model('Workspace', WorkspaceSchema)
+module.exports = mongoose.model('User', UserSchema)
