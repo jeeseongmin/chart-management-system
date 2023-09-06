c8d0d6887c7d4de27c60e68080a98c5bafafe798
jeeseongmin <peration0422@exntu.com>
2023-09-06 22:19:40 +0900

    Feat[#9] : 워크스페이스 헤더 UI 개발

--- front/src/components/layout/ContentsLayout.jsx
+++ front/src/components/layout/ContentsLayout.jsx
@@ -11,12 +11,38 @@ const ContentLayout = ({children}) => {
     <div id={"contentLayout"}>
       <Sidebar/>
       <div id={"workspaceHeader"}>
-        <div style={{display: "flex", alignItems: "center", gap: "16px", border: "1px solid red"}}>
-          <h1 style={{margin: 0, padding: 0}}>My WorkSpace</h1>
-          <div style={{color: "white", backgroundColor: "#35393f", fontWeight: "lighter", padding: "4px 12px"}}>소유자
+        <div style={{
+          width: "80%",
+          height: "3rem",
+          minWidth: "60rem",
+          display: "flex",
+          alignItems: "center",
+          gap: "16px",
+          border: "1px solid red",
+          justifyContent: "space-between"
+        }}>
+          <div
+            style={{
+              display: "flex",
+              alignItems: "center",
+              gap: "16px",
+            }}>
+            <h1 style={{margin: 0, padding: 0}}>My WorkSpace</h1>
+            <div style={{color: "white", backgroundColor: "#35393f", fontWeight: "lighter", padding: "4px 12px"}}>소유자
+            </div>
+          </div>
+          <div>
+            <button style={{
+              padding: "4px 12px",
+              fontWeight: "bold",
+              backgroundColor: "transparent",
+              border: "1px solid transparent",
+              cursor: "pointer",
+            }}>check
+            </button>
           </div>
         </div>
-        <div></div>
+      
       </div>
       <Contents/>
     </div>
